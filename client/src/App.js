e02416900e821b3f4ce722392fda1d53b879781f
Claire Vita <clairej.vita@gmail.com>
2021-01-28 00:00:27 -0500

    section card directories

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import "./App.css";
 import Nav from './components/Nav';
 import Main from './components/Main';
+
 function App() {
   return (
-    <div>
+    <body>
       <Nav />
       <Main />
       <hr class="my-4"></hr>
-    </div>
+    </body>
   );
 }
 
