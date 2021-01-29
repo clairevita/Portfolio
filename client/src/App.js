4395c7323dd5660ec50606f1b4f640ebbc33603b
Claire Vita <clairej.vita@gmail.com>
2021-01-28 23:55:27 -0500

    suitchanging detail

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,33 @@ import Nav from './components/Nav';
 import Main from './components/Main';
 import Section from './components/Section';
 function App() {
+
+  console.log("Welcome to my portfolio! Thanks for taking a look at a console. If you'd like to get in touch with me, head to the Contact page.");
+
+  let suit = "♘"
+
+  const suitChanger = setInterval(changeSuit, 7000);
+  function changeSuit() {
+
+    if (suit == "♘") {
+      $("#suit").text("Claire ♙ Vita");
+      suit = "♙";
+      ;
+    } else if (suit == "♙") {
+      $("#suit").text("Claire ♗ Vita");
+      suit = "♗";
+    } else if (suit == "♗") {
+      $("#suit").text("Claire ♖ Vita");
+      suit = "♖";
+    } else if (suit == "♖") {
+      $("#suit").text("Claire ♔ Vita ");
+      suit = "♔";
+    } else if (suit == "♔") {
+      $("#suit").text("Claire ♘ Vita");
+      suit = "♘";
+    }
+  }
+
   return (
     <body>
       <Nav />
